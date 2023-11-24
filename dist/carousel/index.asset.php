0b50a824cfab4375d145aaa8d44cb89fda3fd4fc
Brett Mason <brett@orphans.co.uk>
2023-11-24 09:49:58 +0000

    Working json validation for advanced controls

--- dist/carousel/index.asset.php
+++ dist/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1c77a2171b0c305aaa8c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '029541a416913eaeec25');
--- src/carousel/components/inspector-controls/advanced-controls.js
+++ src/carousel/components/inspector-controls/advanced-controls.js
@@ -1,24 +1,59 @@
-import { TextareaControl } from '@wordpress/components';
+import { TextareaControl, Notice } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+import { useState, useEffect } from '@wordpress/element';
 
 export default function AdvancedControls({
 	onChange,
 	advancedCarouselSettings,
 }) {
+	const [jsonValid, setJsonValid] = useState(null);
+	const [tempInputValue, setTempInputValue] = useState(
+		JSON.stringify(advancedCarouselSettings, null, 2)
+	);
+
+	const handleInputChange = (value) => {
+		setTempInputValue(value);
+		try {
+			const parsedJson = JSON.parse(value);
+			setJsonValid(true);
+			updateSettings(parsedJson);
+		} catch (error) {
+			setJsonValid(false);
+		}
+	};
+
+	const updateSettings = (parsedJson) => {
+		setTimeout(() => {
+			onChange({
+				advancedCarouselSettings: parsedJson,
+			});
+		}, 500);
+	};
+
+	const jsonText = jsonValid ? __('JSON is valid') : __('JSON is invalid');
+
+	useEffect(() => {
+		handleInputChange(tempInputValue);
+	}, []);
+
 	return (
 		<>
 			<TextareaControl
-				label={__('Carousel settings')}
-				help={__(
-					'Override the carousel settings with a custom Splide JSON object.'
-				)}
-				rows={12}
-				onChange={(value) =>
-					onChange({
-						advancedCarouselSettings: JSON.parse(value),
-					})
+				label={__('Carousel Settings')}
+				help={
+					<Notice
+						isDismissible={false}
+						status={jsonValid ? 'success' : 'error'}
+					>
+						<p style={{ color: '#1e1e1e', margin: 0 }}>
+							{jsonText}
+						</p>
+					</Notice>
 				}
-				value={JSON.stringify(advancedCarouselSettings, null, 2)}
+				rows={12}
+				onChange={(value) => handleInputChange(value)}
+				value={tempInputValue}
+				className={!jsonValid ? 'is-invalid-input' : ''}
 			/>
 		</>
 	);
