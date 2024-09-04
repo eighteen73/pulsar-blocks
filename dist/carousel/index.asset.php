54a80199b37300540531d82372cec3524a851c16
Brett Mason <brett@eighteen73.co.uk>
2024-09-04 15:19:13 +0100

    Fix issue with carousel arrows/pagination not showing

--- dist/carousel/index.asset.php
+++ dist/carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9a598a50418f7c29eecc');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '65c6114a0c20ebc24ce8');
