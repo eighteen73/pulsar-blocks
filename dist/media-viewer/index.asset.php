66810bdd776a3cd6bb15fc3c7408450f4b5a146c
Brett Mason <brett@eighteen73.co.uk>
2025-01-20 14:03:04 +0000

    Fix css for grid layouts and add support for gap

--- dist/media-viewer/index.asset.php
+++ dist/media-viewer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5f72841c05a3b5104b76');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7ec520e6e2a005420dcc');
