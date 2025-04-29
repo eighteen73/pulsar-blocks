84b2dee18146d6c73ac8c38a1ac904eb29a1659a
Brett Mason <brett@eighteen73.co.uk>
2025-04-29 08:57:47 +0100

    Rework block edit to use server side rendering

--- src/menu/edit.js
+++ src/menu/edit.js
@@ -4,10 +4,12 @@ import {
 	SelectControl,
 	Spinner,
 	ToggleControl,
+	Disabled,
 } from '@wordpress/components';
 import { useState, useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
+import ServerSideRender from '@wordpress/server-side-render';
 
 import clsx from 'clsx';
 
@@ -19,22 +21,23 @@ import clsx from 'clsx';
  * @return {Element} Element to render.
  */
 export default function Edit({ attributes, setAttributes }) {
-	const { menuLocation, isResponsive } = attributes;
-	const blockProps = useBlockProps({
-		className: clsx(`is-location-${menuLocation}`, {
-			'is-responsive': attributes.isResponsive,
-		}),
-	});
+	const {
+		menuLocation,
+		isResponsive,
+		hasSubmenuBack,
+		hasSubmenuLabel,
+		menuType,
+		opensOnClick,
+	} = attributes;
+	const blockProps = useBlockProps();
 
 	const [menuLocations, setMenuLocations] = useState([]);
-	const [menuItems, setMenuItems] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 	const [error, setError] = useState(null);
 
-	// Fetch menu locations when the component mounts
 	useEffect(() => {
 		setIsLoading(true);
-		setError(null); // Reset error on new fetch
+		setError(null);
 
 		apiFetch({ path: '/pulsar/v1/menu-locations' })
 			.then((locations) => {
@@ -51,86 +54,25 @@ export default function Edit({ attributes, setAttributes }) {
 			});
 	}, []);
 
-	// Fetch menu items when the menu location changes
-	useEffect(() => {
-		if (menuLocation) {
-			setIsLoading(true);
-			setError(null); // Reset error on new fetch
-
-			apiFetch({ path: `/pulsar/v1/menu-location/${menuLocation}` })
-				.then((items) => {
-					setMenuItems(items);
-					setIsLoading(false);
-				})
-				.catch((fetchError) => {
-					console.error('Error fetching menu items:', fetchError);
-					setError(
-						fetchError.message ||
-							__('Failed to load menu items.', 'pulsar')
-					);
-					setIsLoading(false);
-				});
-		}
-	}, [menuLocation]);
-
-	// Prepare options for the SelectControl
 	const locationOptions = [
 		{ label: __('— Select a Menu Location —', 'pulsar'), value: '' },
 		...menuLocations.map((location) => ({
-			label: location.name, // Use the descriptive name
-			value: location.slug, // Use the slug as the value
+			label: location.name,
+			value: location.slug,
 		})),
 	];
-
-	// Render a list of top level menu items for the selected location
-	const MenuItems = () => {
-		// render menu structure. Each item has a children property with nested items
-		const renderMenuItems = (items) => {
-			return items.map((item) => {
-				return (
-					<li
-						className={
-							item.children
-								? 'wp-block-pulsar-menu__item has-submenu'
-								: 'wp-block-pulsar-menu__item'
-						}
-						key={item.id}
-					>
-						<a
-							className="wp-block-pulsar-menu__link"
-							href={item.url}
-						>
-							{item.title}
-						</a>
-						{item.children && item.children.length > 0 && (
-							<>
-								<button className="wp-block-pulsar-menu__submenu-icon"></button>
-								<ul>{renderMenuItems(item.children)}</ul>
-							</>
-						)}
-					</li>
-				);
-			});
-		};
-
-		return (
-			<ul className="wp-block-pulsar-menu__items">
-				{renderMenuItems(menuItems)}
-			</ul>
-		);
-	};
 
 	return (
 		<>
 			<InspectorControls>
-				<PanelBody title={__('Menu Settings', 'pulsar')}>
+				<PanelBody>
 					{isLoading && <Spinner />}
 					{!isLoading && error && (
 						<p style={{ color: 'red' }}>{error}</p>
 					)}
 					{!isLoading && !error && (
 						<SelectControl
-							label={__('Menu Location', 'pulsar')}
+							label={__('Location', 'pulsar')}
 							value={menuLocation}
 							options={locationOptions}
 							onChange={(newLocation) =>
@@ -144,30 +86,83 @@ export default function Edit({ attributes, setAttributes }) {
 					)}
 					<ToggleControl
 						label={__('Responsive', 'pulsar')}
-						help={__('Enable responsive menu.', 'pulsar')}
+						help={
+							isResponsive
+								? __(
+										'Menu will collapse on smaller screens.',
+										'pulsar'
+									)
+								: __('Menu will not collapse.', 'pulsar')
+						}
 						checked={isResponsive}
 						onChange={(newIsResponsive) =>
 							setAttributes({ isResponsive: newIsResponsive })
 						}
 					/>
 				</PanelBody>
-			</InspectorControls>
-			<nav {...blockProps}>
-				{
-					// Render the menu based on the selected location
-					menuLocation ? (
-						<>
-							<button className="wp-block-pulsar-menu__open">
-								<span className="wp-block-pulsar-menu__open-icon"></span>
-							</button>
 
-							<MenuItems />
-						</>
-					) : (
-						<p>{__('No menu location selected.', 'pulsar')}</p>
-					)
-				}
-			</nav>
+				{isResponsive && (
+					<PanelBody
+						title={__('Responsive Options', 'pulsar')}
+						initialOpen={true}
+					>
+						<SelectControl
+							label={__('Menu Type', 'pulsar')}
+							value={menuType}
+							options={[
+								{
+									label: __('Full Screen', 'pulsar'),
+									value: 'full-screen',
+								},
+								{
+									label: __('Below header', 'pulsar'),
+									value: 'below-header',
+								},
+							]}
+							onChange={(val) => {
+								setAttributes({ menuType: val });
+							}}
+						/>
+						<ToggleControl
+							label={__('Show back button', 'pulsar')}
+							checked={hasSubmenuBack}
+							onChange={(val) => {
+								setAttributes({ hasSubmenuBack: val });
+							}}
+						/>
+						{hasSubmenuBack && (
+							<ToggleControl
+								label={__('Show parent label', 'pulsar')}
+								checked={hasSubmenuLabel}
+								onChange={(val) => {
+									setAttributes({ hasSubmenuLabel: val });
+								}}
+							/>
+						)}
+					</PanelBody>
+				)}
+			</InspectorControls>
+			<div {...blockProps}>
+				{menuLocation ? (
+					<Disabled>
+						<ServerSideRender
+							block="pulsar/menu"
+							attributes={{
+								menuLocation,
+								isResponsive,
+								hasSubmenuBack,
+								hasSubmenuLabel,
+								menuType,
+								opensOnClick,
+							}}
+						/>
+					</Disabled>
+				) : (
+					<p className="wp-block-pulsar-menu__placeholder">
+						{__('Select a menu location to display.', 'pulsar')}
+					</p>
+				)}
+			</div>
 		</>
 	);
 }
